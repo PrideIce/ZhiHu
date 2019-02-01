d605c910d43acab63536f77156d9e9e6b6b5479d
陈逸辰 <chenyichen@chenyichens-iMac.local>
2019-02-01 17:30:36 +0800

    获取数据

--- ZhiHu/ZhiHu/FirstViewController.swift
+++ ZhiHu/ZhiHu/FirstViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class FirstViewController: UIViewController {
 
@@ -21,5 +22,20 @@ class FirstViewController: UIViewController {
     }
 
 
+    @IBAction func demo(_ sender: Any) {
+        Alamofire.request("https://api.zhihu.com/topstory/follow?limit=10&reverse_order=0&start_type=warm").responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+    }
 }
 
