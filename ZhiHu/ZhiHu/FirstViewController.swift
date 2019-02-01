13e0087e47d0bed4d65a4566a8788c4d7630a21b
陈逸辰 <chenyichen@chenyichens-iMac.local>
2019-02-01 15:52:05 +0800

    创建工程

---
+++ ZhiHu/ZhiHu/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ZhiHu
+//
+//  Created by 陈逸辰 on 2019/2/1.
+//  Copyright © 2019 陈逸辰. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ZhiHu/ZhiHu/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ZhiHu
+//
+//  Created by 陈逸辰 on 2019/2/1.
+//  Copyright © 2019 陈逸辰. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
